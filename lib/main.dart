223ed4499663df599686713cefeb38a4d4a12f83
nguetcheu <dnguetcheu@gmail.com>
2024-03-18 12:47:28 +0100

    retrait du messsage banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,13 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Academy Manager'),
+        ),
+      ),
+    );
   }
 }
